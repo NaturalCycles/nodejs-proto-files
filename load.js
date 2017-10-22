2ad36ba1d4dc024f207b93debd11ce2509ff14ba
Okan Cetin <okancetin44@gmail.com>
2017-10-22 16:18:43 +0200

    fix load func

--- load.js
+++ load.js
@@ -4,48 +4,36 @@ var globby = require('globby')
 var path = require('path')
 var protobuf = require('protobufjs')
 
-var COMMON_PROTO_DIRS = [
-  // This list of directories is defined here:
-  // https://github.com/googleapis/googleapis/blob/master/gapic/packaging/common_protos.yaml
-  'api',
-  path.join('logging', 'type'),
-  'longrunning',
-  'protobuf', // This is an additional path that the common protos depend on.
-  'rpc',
-  'type'
-]
-
-var COMMON_PROTO_GLOB_PATTERNS = COMMON_PROTO_DIRS.map(function (dir) {
-  return path.join('google', dir, '**', '*.proto')
-})
-
+var COMMON_PROTO_GLOB_PATTERNS = path.join(__dirname, 'google', '**', '*.proto')
 var COMMON_PROTO_FILES = globby.sync(COMMON_PROTO_GLOB_PATTERNS)
 
 class GoogleProtoFilesRoot extends protobuf.Root {
-  constructor () {
+  constructor() {
     super([].slice.apply(arguments))
   }
 
   // Causes the loading of an included proto to check if it is a common
   // proto. If it is a common proto, use the google-proto-files proto.
-  resolvePath (_, includePath) {
+  resolvePath(_, includePath) {
     // Fully qualified paths don't need to be resolved.
     if (includePath.startsWith('/')) {
       return includePath
     }
 
-    if (COMMON_PROTO_FILES.indexOf(includePath) > -1) {
-      return path.join(__dirname, includePath)
+    for (var i = 0, len = COMMON_PROTO_FILES.length; i < len; i++) {
+      if (COMMON_PROTO_FILES[i].indexOf(includePath) > -1) {
+        return COMMON_PROTO_FILES[i]
+      }
     }
+
     return protobuf.util.path.resolve.apply(null, [].slice.call(arguments))
   }
 }
 
-module.exports.loadSync = function (filename) {
+module.exports.loadSync = function(filename) {
   return protobuf.loadSync(filename, new GoogleProtoFilesRoot())
 }
 
-module.exports.load = function (filename, callback) {
+module.exports.load = function(filename, callback) {
   return protobuf.load(filename, new GoogleProtoFilesRoot())
 }
-
